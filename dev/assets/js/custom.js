6f033426a732e7405cf9f3b9ef1d935555456840
ArtemBudnyk <qrtleo@gmail.com>
2023-12-11 16:53:17 +0200

    form for testing

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,31 +1,7 @@
 
 
-// function checkAnswer() {
-//     // Получаем все радиокнопки с именем "question"
-//     var radioButtons = document.getElementsByName('question');
-
-//     // Переменная для хранения выбранного ответа
-//     var selectedAnswer = null;
-
-//     // Перебираем радиокнопки
-//     for (var i = 0; i < radioButtons.length; i++) {
-//         if (radioButtons[i].checked) {
-//             // Если кнопка выбрана, сохраняем ее значение
-//             selectedAnswer = radioButtons[i].value;
-//             break;
-//         }
-//     }
-
-//     // Проверяем правильность ответа и выводим сообщение
-//     if (selectedAnswer === 'option2') {
-//         alert('Правильно!');
-//     } else {
-//         alert('Yt ghfdbkmyj');
-//     }
-// }
-
-
 // Вопрос 1
+
 
 function checkAnswerOne() {
     var radioButtons = document.getElementsByName('question');
@@ -48,9 +24,9 @@ function checkAnswerOne() {
 
     console.log(selectedAnswerOne);       // нужно
 
-    // setTimeout(function() {
-    //     window.location.href = '2-question.html'; // Замените на фактический путь
-    // }, 3000);
+    setTimeout(function() {
+        window.location.href = 'form-2.html'; // Замените на фактический путь
+    }, 3000);
 }
 
 
@@ -66,28 +42,112 @@ function showIncorrectAlert() {
 
 // Вопрос 2
 
-// function checkAnswerTwo() {
-//     var radioButtons = document.getElementsByName('question');
-//     var selectedAnswerOne = null;
+function checkAnswerTwo() {
+    var radioButtons = document.getElementsByName('question');
+    var selectedAnswerOne = null;
 
-//     for (var i = 0; i < radioButtons.length; i++) {
-//         if (radioButtons[i].checked) {
-//             selectedAnswerOne = radioButtons[i].value;
-//             break;
-//         }
-//     }
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            selectedAnswerOne = radioButtons[i].value;
+            break;
+        }
+    }
 
-//     if (selectedAnswerOne === 'option2.1') {
-//         // Ответ верный
-//         showCorrectAlert();
-//     } else {
-//         // Ответ неверный
-//         showIncorrectAlert();
-//     }
+    if (selectedAnswerOne === 'option2.1') {
+        // Ответ верный
+        showCorrectAlert();
+    } else {
+        // Ответ неверный
+        showIncorrectAlert();
+    }
 
-//     console.log(selectedAnswerOne);       // нужно
+    console.log(selectedAnswerOne);       // нужно
 
-    // setTimeout(function() {
-    //     window.location.href = '2-question.html'; // Замените на фактический путь
-    // }, 3000);
-// }
+    setTimeout(function() {
+        window.location.href = 'form-3.html'; // Замените на фактический путь
+    }, 3000);
+}
+
+// Вопрос 3
+
+function checkAnswerThree() {
+    var radioButtons = document.getElementsByName('question');
+    var selectedAnswerOne = null;
+
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            selectedAnswerOne = radioButtons[i].value;
+            break;
+        }
+    }
+
+    if (selectedAnswerOne === 'option3.3') {
+        // Ответ верный
+        showCorrectAlert();
+    } else {
+        // Ответ неверный
+        showIncorrectAlert();
+    }
+
+    console.log(selectedAnswerOne);       // нужно
+
+    setTimeout(function() {
+        window.location.href = 'form-4.html'; // Замените на фактический путь
+    }, 3000);
+}
+
+// Вопрос 4
+
+function checkAnswerFour() {
+    var radioButtons = document.getElementsByName('question');
+    var selectedAnswerOne = null;
+
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            selectedAnswerOne = radioButtons[i].value;
+            break;
+        }
+    }
+
+    if (selectedAnswerOne === 'option4.4') {
+        // Ответ верный
+        showCorrectAlert();
+    } else {
+        // Ответ неверный
+        showIncorrectAlert();
+    }
+
+    console.log(selectedAnswerOne);       // нужно
+
+    setTimeout(function() {
+        window.location.href = 'form-5.html'; // Замените на фактический путь
+    }, 3000);
+}
+
+// Вопрос 5
+
+function checkAnswerFive() {
+    var radioButtons = document.getElementsByName('question');
+    var selectedAnswerOne = null;
+
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            selectedAnswerOne = radioButtons[i].value;
+            break;
+        }
+    }
+
+    if (selectedAnswerOne === 'option5.4') {
+        // Ответ верный
+        showCorrectAlert();
+    } else {
+        // Ответ неверный
+        showIncorrectAlert();
+    }
+
+    console.log(selectedAnswerOne);       // нужно
+
+    setTimeout(function() {
+        window.location.href = 'form-6.html'; // Замените на фактический путь
+    }, 3000);
+}
